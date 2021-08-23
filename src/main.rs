8848e54a47ae1de124f0c64dca5c0efbea49ebf4
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-08-23 17:24:07 +0000

    hehe this is cool

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpServer, Result};
+use actix_files::{NamedFile, Files};
 use r2d2_sqlite::{self, SqliteConnectionManager};
 
 mod db;
 use db::{Pool};
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn hello() -> Result<NamedFile> {
+    Ok(NamedFile::open("dist/index.html")?)
 }
 
 #[actix_web::main]
@@ -18,6 +19,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .data(pool.clone())
             .service(hello)
+            .service(Files::new("/assets", "dist/assets").show_files_listing())
     })
     .bind("127.0.0.1:8080")?
     .run()
