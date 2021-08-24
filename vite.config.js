1a7ce2ab189b6098c6bb41827fb5a0465afd9c14
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-08-24 13:00:43 -0400

    windicss?

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import WindiCSS from "vite-plugin-windicss"
 
 export default defineConfig({
-  root: "frontend",
-  plugins: [solidPlugin()],
-  build: {
-    target: "esnext",
-    outDir: "../dist"
-  },
+    root: "frontend",
+    plugins: [
+        solidPlugin(),
+        WindiCSS({
+            scan: {
+                fileExtensions: ["html", "js", "ts", "jsx", "tsx"]
+            },
+        })
+    ],
+    build: {
+        target: "esnext",
+        outDir: "../dist"
+    },
 });
