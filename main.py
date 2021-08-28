d08c6251b90bed13523ed8263584c40dc2c11108
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-08-28 00:44:05 +0000

    lmao this is so much easier than rust

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from starlette.applications import Starlette
 from starlette.routing import Route, Mount
 from starlette.staticfiles import StaticFiles
-from starlette.responses import Response, RedirectResponse
+from starlette.responses import Response, RedirectResponse, JSONResponse
 from starlette.requests import Request
 
 
+from random import choices
+from string import hexdigits
+
+base_url = "https://mzf.one/"
+
+async def set_url(request: Request) -> JSONResponse:
+    data = await request.json()
+    url = data.get("url")
+    if url is None:
+        return JSONResponse({"message": "No URL provided."}, status_code=400)
+    
+    key = "".join(choices(hexdigits, k=5))
+
+    return JSONResponse({"key": key})
+
+
 routes = [
+    Route("/set", endpoint=set_url, methods=["POST"]),
     Mount("/", StaticFiles(directory="dist/", html=True))
 ]
 
